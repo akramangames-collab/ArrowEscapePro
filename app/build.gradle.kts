8fe003e09f5109da9155967b2e284109caff1704
akramangames-collab <akramangames@gmail.com>
2026-09-07 12:44:07 +0530

    Make every 5th level a SUPER HARD milestone

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,8 +7,8 @@ android {
         applicationId = "com.arrowescape.pro"
         minSdk = 23
         targetSdk = 36
-        versionCode = 18
-        versionName = "16.0.1"
+        versionCode = 19
+        versionName = "16.0.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
