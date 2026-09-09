65dcbadacf64d02137e9bd1161e2589245a9e092
akramangames-collab <akramangames@gmail.com>
2026-09-09 15:19:44 +0530

    V19: bump Android version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,8 +7,8 @@ android {
         applicationId = "com.arrowescape.pro"
         minSdk = 23
         targetSdk = 36
-        versionCode = 23
-        versionName = "18.0.0"
+        versionCode = 24
+        versionName = "19.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
